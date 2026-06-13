eb519988a18f941b81c0694ef450e008b290752c
Geusan Edurais <edurais.ariadaffa@gmail.com>
2026-06-13 19:23:49 +0700

    Update package-lock.json and tsconfig.app.json; refactor vite.config.ts for consistency

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 // vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // Daftarkan plugin tailwind disini agar compiler membaca @import "tailwindcss"
-    resolve: {
+  plugins: [react(), tailwindcss()], // Daftarkan plugin tailwind disini agar compiler membaca @import "tailwindcss"
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  ],
 });
